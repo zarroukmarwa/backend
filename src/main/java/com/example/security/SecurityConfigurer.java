801379b491c9d5932406650e8a4a99b5897dc13c
marwa zarrouk <zarouk.maroua@gmail.com>
2021-01-29 09:49:29 +0100

    Update SecurityConfigurer.java

--- src/main/java/com/example/security/SecurityConfigurer.java
+++ src/main/java/com/example/security/SecurityConfigurer.java
@@ -23,15 +23,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.authorizeRequests().antMatchers("/login", "/h2-console/**", "/api/users").permitAll()
-				.anyRequest().permitAll()
-				.and()
-				.httpBasic()
-				.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-		http.headers().frameOptions().disable();
+		http.cors();
 
 		
 	} 
